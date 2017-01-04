274b536d7cd53fcd08d15336cc40e891c7d72f03
aj-t-swoboda <aj.t.swoboda@gmail.com>
2017-01-03 22:25:28 -0800

    added salary model to salaries app

--- xsoccer/salaries/models.py
+++ xsoccer/salaries/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Salary(models.Model):
+    """
+    An Opta player salary. 
+    """
+    #this FK has a cascade on_delete setting, as without player info, this data is useless
+    player = models.ForeignKey('players.Player', on_delete=models.CASCADE, null=False, related_name="player_tied_to_salary")
+
+    season = models.IntegerField("Season", null=False)
+    
+    base_salary = models.FloatField("Base Salary ($)", default=0.0, null=True)
+    compensation = models.FloatField("Total Compensation ($)", default=0.0, null=True)
+
+    def __str__(self):
+        return "%s ($%s;$%s in %s)" % (self.player, self.base_salary, self.compensation, self.season)
