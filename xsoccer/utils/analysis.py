8550c721eebf6bae88367edea9a77312ed18b196
aj-t-swoboda <aj.t.swoboda@gmail.com>
2017-01-14 14:16:41 -0800

    added another function

--- xsoccer/utils/analysis.py
+++ xsoccer/utils/analysis.py
@@ -10,7 +10,11 @@ games = ua.list_games(p)
 
 stats = ua.game_stats(p, games[0])
 
+ua.statistic_values(p,games[0],["passes_left", "accurate_pass", "aerial_won", "aerial_lost", "accurate_back_zone_pass", "accurate_fwd_zone_pass"])
+ua.statistic_values(p,games[0],["passes_left"])
+
 """
+import time
 
 from eventstatistics.models import EventStatistic
 from games.models import Game
@@ -22,6 +26,7 @@ from statistictypes.models import StatisticType
 from teams.models import Team
 from teamstatistics.models import TeamStatistic
 from venues.models import Venue
+
 
 def list_games(player):
 	"""Return a list of Game instances for a Player input"""
@@ -44,3 +49,20 @@ def game_stats(player, game):
 		stats.append(stat)
 
 	return stats
+
+def statistic_values(player, game, list_desired_stats):
+	"""Return the value(s) of the specific statistic(s) for a player / game"""
+	#Note: inputting a list of stats at once, as calling game_stats only once 
+	#will save us dramatically in reducing hits to the database
+	stats = game_stats(player, game)
+
+	values = {}
+
+	for desired_stat in list_desired_stats:
+		values[desired_stat] = 0.0
+
+		for s in stats:
+			if s.statistic.opta_statistic_type_name == desired_stat:
+				values[desired_stat] = s.value
+	
+	return values
